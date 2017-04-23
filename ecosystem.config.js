2a83bcced920f802fa5437f2d592690416fd2e13
thm <2@qq.com>
2017-04-23 22:16:32 +0800

    增加文件

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'pm2app',
+      script    : '/bin/www',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'mingetian',
+      host : 'git@github.com:mingetian',
+      ref  : 'origin/master',
+      repo : 'git@github.com:mingetian/pm2-using.git',
+      path : '/home/thm/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
